2628f15881d6ccd894b43faf1e28700866b761ec
Jorge Ramos <v-jorramos@expediagroup.com>
2023-01-03 14:57:18 -0600

    Add example to show the difference between ref and reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Reactive vs Ref</h1>
+    <h3>{{ ironman }}</h3>
+    <h3>{{ hulk }}</h3>
+    <button @click="changeIronman()">Change Ironman</button>
+    <button @click="changeHulk()">Change Hulk</button>
+    <h3>{{ isIronmanActive }}</h3>
+    <h3>{{ isHulkActive }}</h3>
+    <button @click="changeStatus()">Change status</button>
   </div>
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
+
 export default {
   name: 'Home',
+  setup() {
+    const ironman = ref({ name: 'Tony', age: 40 });
+    const hulk = reactive({ name: 'Bruce', age: 45 });
+    const isIronmanActive = ref(true);
+    const isHulkActive = reactive(true);
+
+    return {
+      ironman,
+      hulk,
+      isIronmanActive,
+      isHulkActive,
+      changeIronman: () => {
+        ironman.value.name = 'Tony Stark';
+        ironman.value.age = 30;
+      },
+      changeHulk: () => {
+        hulk.name = 'Bruce Banner';
+        hulk.age = 35;
+      },
+      changeStatus: () => {
+        isIronmanActive.value = !isIronmanActive.value;
+        isHulkActive = !isHulkActive; // It will show an error
+      },
+    };
+  },
 };
 </script>
