5bfaf53643f42a2fba7ee89fa5827bcc50992c26
Jorge Ramos <v-jorramos@expediagroup.com>
2023-01-31 09:52:04 -0600

    Implement getter with arguments

--- src/store/index.js
+++ src/store/index.js
@@ -9,14 +9,26 @@ export default createStore({
     ],
   },
   getters: {
-    allTodos(state, getters, rootState) {
+    allTodos: (state, getters, rootState) => {
       return [...state.todos];
     },
-    pendingTodos(state, getters, rootState) {
+    pendingTodos: (state, getters, rootState) => {
       return state.todos.filter(todo => !todo.completed);
     },
-    completedTodos(state, getters, rootState) {
+    completedTodos: (state, getters, rootState) => {
       return state.todos.filter(todo => todo.completed);
+    },
+    getTodosByTab: (_, getters) => tab => {
+      switch (tab) {
+        case 'all':
+          return getters.allTodos;
+        case 'pending':
+          return getters.pendingTodos;
+        case 'completed':
+          return getters.completedTodos;
+        default:
+          return getters.allTodos;
+      }
     },
   },
   mutations: {},
--- src/views/TodosVuexView.vue
+++ src/views/TodosVuexView.vue
@@ -1,31 +1,85 @@
 <template>
   <div class="todos">
     <h1>Todos Vuex View</h1>
-    <h2>All tasks: {{ allTodos.length }}</h2>
-    <hr />
     <h2>Pending tasks: {{ pendingTodos.length }}</h2>
     <hr />
-    <h2>Completed tasks: {{ completedTodos.length }}</h2>
-    <hr />
+    <button
+      @click="currentTab = 'all'"
+      :class="{ active: currentTab === 'all' }"
+    >
+      All
+    </button>
+    <button
+      @click="currentTab = 'pending'"
+      :class="{ active: currentTab === 'pending' }"
+    >
+      Pending
+    </button>
+    <button
+      @click="currentTab = 'completed'"
+      :class="{ active: currentTab === 'completed' }"
+    >
+      Completed
+    </button>
+    <div class="todo-list">
+      <ul>
+        <li
+          v-for="todo in getTodosByTab"
+          :key="todo.id"
+          :class="{ completed: todo.completed }"
+        >
+          {{ todo.text }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 
 export default {
   name: 'TodosVuexView',
   setup() {
     const store = useStore();
+    const currentTab = ref('all');
 
     return {
+      currentTab,
       allTodos: computed(() => store.getters['allTodos']),
       completedTodos: computed(() => store.getters['completedTodos']),
       pendingTodos: computed(() => store.getters['pendingTodos']),
+      getTodosByTab: computed(() =>
+        store.getters['getTodosByTab'](currentTab.value)
+      ),
     };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+div.todo-list {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+
+ul {
+  text-align: left;
+  width: 300px;
+}
+
+li {
+  cursor: pointer;
+}
+
+.active {
+  background-color: #2c3e50;
+  color: white;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+</style>
