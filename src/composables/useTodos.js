7a4273641d0e73fce8a001cacddb8c5ce3ed615a
Jorge Ramos <v-jorramos@expediagroup.com>
2023-02-02 09:56:15 -0600

    Add useTodos composable

---
+++ src/composables/useTodos.js
@@ -0,0 +1,22 @@
+import { computed, ref } from 'vue';
+import { useStore } from 'vuex';
+
+const useTodos = () => {
+  const store = useStore();
+  const currentTab = ref('all');
+
+  return {
+    currentTab,
+    allTodos: computed(() => store.getters['allTodos']),
+    completedTodos: computed(() => store.getters['completedTodos']),
+    pendingTodos: computed(() => store.getters['pendingTodos']),
+    getTodosByTab: computed(() =>
+      store.getters['getTodosByTab'](currentTab.value)
+    ),
+    toggleTodo: id => {
+      store.commit('toggleTodo', id);
+    },
+  };
+};
+
+export default useTodos;
