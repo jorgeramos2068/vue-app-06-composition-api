c30878bf72615eaa20be205728f356edf7bef407
Jorge Ramos <v-jorramos@expediagroup.com>
2023-02-03 12:37:13 -0600

    Add example to show the use of slots

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     <router-link :to="{ name: 'counter' }">Counter</router-link> |
     <router-link :to="{ name: 'users' }">Users</router-link> |
     <router-link :to="{ name: 'search' }">Search Pokemon</router-link> |
-    <router-link :to="{ name: 'todos' }">Todos</router-link>
+    <router-link :to="{ name: 'todos' }">Todos</router-link> |
+    <router-link :to="{ name: 'slots' }">Slots</router-link>
   </nav>
   <router-view v-slot="{ Component, route }">
     <keep-alive>
---
+++ src/views/CustomSlotsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Slots</h1>
+  <button>Open Modal</button>
+  <modal>
+    <template v-slot:body>
+      <p>Lorem ipsum</p>
+    </template>
+    <template v-slot:footer>
+      <button>Close</button>
+    </template>
+    <template v-slot:header>
+      <h2>Modal title (named)</h2>
+    </template>
+  </modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal';
+
+export default {
+  components: { Modal },
+  name: 'CustomSlotsView',
+  setup() {},
+};
+</script>
+
+<style scoped></style>
