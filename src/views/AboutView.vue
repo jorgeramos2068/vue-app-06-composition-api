e9893fadd9d17a543953b6c1978359f806eba105
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:40:49 -0600

    Add component to show the use of lifecycle methods

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,72 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Lifecycle Hooks</h1>
   </div>
 </template>
+
+<script>
+import {
+  onBeforeMount,
+  onMounted,
+  onActivated,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onDeactivated,
+  onErrorCaptured,
+  onRenderTracked,
+  onRenderTriggered,
+  onUnmounted,
+  onUpdated,
+} from '@vue';
+
+export default {
+  name: 'About',
+  setup() {
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+
+    onMounted(() => {
+      console.log('onMounted');
+    });
+
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+
+    onErrorCaptured(() => {
+      console.log('onErrorCaptured');
+    });
+
+    onRenderTracked(() => {
+      console.log('onRenderTracked');
+    });
+
+    onRenderTriggered(() => {
+      console.log('onRenderTriggered');
+    });
+
+    onActivated(() => {
+      console.log('onActivated');
+    });
+
+    onDeactivated(() => {
+      console.log('onDeactivated');
+    });
+  },
+};
+</script>
+
+<style></style>
